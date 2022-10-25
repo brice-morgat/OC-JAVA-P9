16c388308fd1cfd1831efb13c4ff2843055ab6a4
brice-morgat <brice.morgat@epitech.eu>
2022-10-25 12:17:10 +0200

    Modify getPatient for return an Optional and change service for getPatientById to return value or elseThrow

--- mediscreen-backend/patient/src/main/java/com/mediscreen/patient/repository/PatientRepo.java
+++ mediscreen-backend/patient/src/main/java/com/mediscreen/patient/repository/PatientRepo.java
@@ -4,7 +4,9 @@ import com.mediscreen.patient.models.Patient;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface PatientRepo extends JpaRepository<Patient, Long> {
-
+    public Optional<Patient> getPatientById(Long id);
 }
--- mediscreen-backend/patient/src/main/java/com/mediscreen/patient/services/impl/PatientServiceImpl.java
+++ mediscreen-backend/patient/src/main/java/com/mediscreen/patient/services/impl/PatientServiceImpl.java
@@ -1,2 +1,46 @@
-package com.mediscreen.patient.services.impl;public class PatientServiceImpl {
+package com.mediscreen.patient.services.impl;
+
+import com.mediscreen.patient.models.Patient;
+import com.mediscreen.patient.repository.PatientRepo;
+import com.mediscreen.patient.services.IPatientService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class PatientServiceImpl implements IPatientService {
+
+    @Autowired
+    PatientRepo patientRepo;
+
+    private final Logger logger = LoggerFactory.getLogger(PatientServiceImpl.class);
+
+    @Override
+    public Patient getPatient(Long id) {
+        return patientRepo.getPatientById(id).orElseThrow();
+    }
+
+    @Override
+    public Patient savePatient(Patient patient) {
+        return patientRepo.save(patient);
+    }
+
+    @Override
+    public void deletePatient(Long id) {
+        Patient patient = patientRepo.getPatientById(id).orElseThrow();
+        patientRepo.delete(patient);
+    }
+
+    @Override
+    public List<Patient> getAllPatients() {
+        return patientRepo.findAll();
+    }
+
+    @Override
+    public Patient updatePatient(Patient patient) {
+        return patientRepo.save(patient);
+    }
 }
